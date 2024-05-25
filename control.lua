6a74b7280ea25d4573a970b3fb0edfa1e85841a3
luiseeo <luiseeo@gmail.com>
2024-05-24 22:28:04 -0700

    Add additional items in the debris in freeplay

---
+++ control.lua
@@ -0,0 +1,24 @@
+-- Change the items available in the Spaceship debris
+script.on_init(function (event)
+
+  -- Items on the main spaceship entity
+  remote.call("freeplay", "set_ship_items", {
+    -- Keep the 8 yellow magazines from vanilla
+    ["firearm-magazine"] = 8,
+    -- Add a basic construction bot kit for Underhaul
+    ["personal-roboport-equipment"] = 1,
+    ["construction-robot"] = 7,
+    ["solar-panel-equipment"] = 3,
+    ["battery-equipment"] = 1
+  })
+
+  -- Items scattered in the small debris entities
+  remote.call("freeplay", "set_debris_items", {
+    -- Keep the 8 iron plates from vanilla
+    ["iron-plate"] = 8,
+    -- Add some belts and solid fuel for Underhaul
+    ["transport-belt"] = 20,
+    ["solid-fuel"] = 16
+  })
+
+end)
